9646fbdd5d9ec2bd8aa3d91dea782f67a62689b7
deshollow <555demin@gmail.com>
2024-01-10 19:57:29 +0300

    Initial Commit

---
+++ VampiresVSHunters/VampiresVSHuntersApp.swift
@@ -0,0 +1,17 @@
+//
+//  VampiresVSHuntersApp.swift
+//  VampiresVSHunters
+//
+//  Created by deshollow on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct VampiresVSHuntersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
