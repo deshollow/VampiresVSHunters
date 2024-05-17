1d3833f9e9001074d43c0d1ac7177b93fe315a6f
deshollow <555demin@gmail.com>
2024-05-17 12:11:45 +0300

    picker timer logic correct now

--- VampiresVSHunters/Models/TimerSettings.swift
+++ VampiresVSHunters/Models/TimerSettings.swift
@@ -5,60 +5,60 @@
 //  Created by deshollow on 16.02.2024.
 //
 
-import SwiftUI
-import Combine
-
-class TimerSettings: ObservableObject {
-    
-    @Published var selectedTime: Int?
-    
-    init(initialSelectedTime: Int?) {
-        self.selectedTime = initialSelectedTime
-    }
-    func updateSelectedTime(forKey: String) {
-        UserDefaults.standard.set(selectedTime, forKey: forKey)
-//        print(selectedTime)
-    }
-    func loadSettings(forKey: String) {
-        if let savedSettings = UserDefaults.standard.object(forKey: forKey) as? Int {
-            selectedTime = savedSettings
-        }
-        selectedTime = 3
-    }
-}
-
-//I - класс нужен, если использую UserDefaults и не нужен если AppStorage
-
 //import SwiftUI
 //import Combine
 //
 //class TimerSettings: ObservableObject {
-//
+//    
 //    @Published var selectedTime: Int?
-//
+//    
 //    init(initialSelectedTime: Int?) {
 //        self.selectedTime = initialSelectedTime
 //    }
-//    //униварсальная функция сохранения
-//    func saveSettings(time: Int, forKey: String) {
-//        UserDefaults.standard.set(time, forKey: forKey)
-//        print(time)
+//    func updateSelectedTime(forKey: String) {
+//        UserDefaults.standard.set(selectedTime, forKey: forKey)
+////        print(selectedTime)
 //    }
-//    //универсальная функция загрузки
-//    func loadSettings(forKey: String) -> Int? {
+//    func loadSettings(forKey: String) {
 //        if let savedSettings = UserDefaults.standard.object(forKey: forKey) as? Int {
 //            selectedTime = savedSettings
-//            return savedSettings
-//        } else {
-//            selectedTime = nil
-//            return nil
 //        }
+//        selectedTime = 3
 //    }
 //}
 
+//I - класс нужен, если использую UserDefaults и не нужен если AppStorage
+
+import SwiftUI
+import Combine
+
+class TimerSettings: ObservableObject {
+
+    @Published var selectedTime: Int?
+
+    init(initialSelectedTime: Int?) {
+        self.selectedTime = initialSelectedTime
+    }
+    //униварсальная функция сохранения
+    func saveSettings(time: Int, forKey: String) {
+        UserDefaults.standard.set(time, forKey: forKey)
+        print(time)
+    }
+    //универсальная функция загрузки
+    func loadSettings(forKey: String) -> Int? {
+        if let savedSettings = UserDefaults.standard.object(forKey: forKey) as? Int {
+            selectedTime = savedSettings
+            return savedSettings
+        } else {
+            selectedTime = nil
+            return nil
+        }
+    }
+}
+
 //II
 
-
+//
 //import SwiftUI
 //import Combine
 //
--- VampiresVSHunters/Views/TimerView.swift
+++ VampiresVSHunters/Views/TimerView.swift
@@ -5,91 +5,93 @@
 //  Created by deshollow on 21.01.2024.
 //
 
-import SwiftUI
-
-struct TimerView: View {
-
-    @StateObject var timerSettings = TimerSettings(initialSelectedTime: nil)
-
-    var body: some View {
-            VStack {
-                Text("Настройте таймер игры:")
-                    .foregroundStyle(Color(.black).opacity(0.8))
-                Picker("Время таймера", selection: $timerSettings.selectedTime) {
-                    ForEach(1..<31, id: \.self) {
-                        Text("\($0) мин").tag($0)
-                    }
-                }.pickerStyle(WheelPickerStyle())
-                    .frame(width: 300, height: 300)
-                Button("Сохранить") {
-                    timerSettings.updateSelectedTime(forKey: "timerSettings")
-                }
-                .font(.title)
-                .foregroundColor(.blue)
-                .padding(.bottom, 100)
-                .onAppear {
-                    timerSettings.loadSettings(forKey: "timerSettings")
-                }
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
-            .navigationBarBackButtonHidden(true)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    CustomBackButtonView()
-                }
-            }
-        }
-    }
-
-#Preview {
-    TimerView()
-}
+//import SwiftUI
+//
+//struct TimerView: View {
+//
+//    @StateObject var timerSettings = TimerSettings(initialSelectedTime: nil)
+//
+//    var body: some View {
+//            VStack {
+//                Text("Настройте таймер игры:")
+//                    .foregroundStyle(Color(.black).opacity(0.8))
+//                Picker("Время таймера", selection: $timerSettings.selectedTime) {
+//                    ForEach(1..<31, id: \.self) {
+//                        Text("\($0) мин").tag($0)
+//                    }
+//                }.pickerStyle(WheelPickerStyle())
+//                    .frame(width: 300, height: 300)
+//                Button("Сохранить") {
+//                    timerSettings.updateSelectedTime(forKey: "timerSettings")
+//                }
+//                .font(.title)
+//                .foregroundColor(.blue)
+//                .padding(.bottom, 100)
+//                .onAppear {
+//                    timerSettings.loadSettings(forKey: "timerSettings")
+//                }
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+//            .navigationBarBackButtonHidden(true)
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    CustomBackButtonView()
+//                }
+//            }
+//        }
+//    }
+//
+//#Preview {
+//    TimerView()
+//}
     
     //I - застрял на сохранении пикера
     
-    //import SwiftUI
-    //
-    //struct TimerView: View {
-    //
-    //    @State var timerSettings = TimerSettings(initialSelectedTime: nil)
-    //
-    //    @State var selectedTime = 0
-    //    @State var mainTime = 3
-    //
-    //    var body: some View {
-    //            VStack {
-    //                Text("Настройте таймер игры:")
-    //                    .foregroundStyle(Color(.black).opacity(0.8))
-    //                Picker("Время таймера", selection: $mainTime) {
-    //                    ForEach(1..<31, id: \.self) {
-    //                        Text("\($0) мин").tag($0)
-    //                    }
-    //                }.pickerStyle(WheelPickerStyle())
-    //                    .frame(width: 300, height: 300)
-    //                Button("Сохранить") {
-    //                    selectedTime = mainTime
-    //                    timerSettings.saveSettings(time: selectedTime, forKey: "timerSettings")
-    //                    mainTime = timerSettings.loadSettings(forKey: "timerSettings")!
-    //                }
-    //                .font(.title)
-    //                .foregroundColor(.blue)
-    //                .padding(.bottom, 100)
-    //            }
-    //            .frame(maxWidth: .infinity, maxHeight: .infinity)
-    //            .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
-    //            .navigationBarBackButtonHidden(true)
-    //            .toolbar {
-    //                ToolbarItem(placement: .navigationBarLeading) {
-    //                    CustomBackButtonView()
-    //                }
-    //            }
-    //        }
-    //    }
-    //
-    //#Preview {
-    //    TimerView()
-    //}
+    import SwiftUI
+    
+    struct TimerView: View {
+    
+        @State var timerSettings = TimerSettings(initialSelectedTime: nil)
+    
+        @State var selectedTime = 0
+        @State var mainTime = 3
+    
+        var body: some View {
+                VStack {
+                    Text("Настройте таймер игры:")
+                        .foregroundStyle(Color(.black).opacity(0.8))
+                    Picker("Время таймера", selection: $mainTime) {
+                        ForEach(1..<31, id: \.self) {
+                            Text("\($0) мин").tag($0)
+                        }
+                    }.pickerStyle(WheelPickerStyle())
+                        .frame(width: 300, height: 300)
+                    Button("Сохранить") {
+                        selectedTime = mainTime
+                        timerSettings.saveSettings(time: selectedTime, forKey: "timerSettings")
+                    }
+                    .font(.title)
+                    .foregroundColor(.blue)
+                    .padding(.bottom, 100)
+                }
+                .onAppear {
+                    mainTime = timerSettings.loadSettings(forKey: "timerSettings")!
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+                .navigationBarBackButtonHidden(true)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        CustomBackButtonView()
+                    }
+                }
+            }
+        }
+    
+    #Preview {
+        TimerView()
+    }
     
     //II StackOver - способ через AppStorage - работает но странный
     
@@ -120,62 +122,62 @@ struct TimerView: View {
     
     //III Спосб с UserDefaults
     
-    //class TimerSettings: ObservableObject {
-    //
-    //    @Published var selectedTime: Int?
-    //
-    //    init(initialSelectedTime: Int?) {
-    //        self.selectedTime = initialSelectedTime
-    //    }
-    //    func updateSelectedTime(forKey: String) {
-    //        UserDefaults.standard.set(selectedTime, forKey: forKey)
-    //        print(time)
-    //    }
-    //    func loadSettings(forKey: String) {
-    //        if let savedSettings = UserDefaults.standard.object(forKey: forKey) as? Int {
-    //            selectedTime = savedSettings
-    //            return savedSettings
-    //        }
-    //        selectedTime = 3
-    //    }
-    //}
-    //
-    //struct TimerView: View {
-    //
-    //    @StateObject var timerSettings = TimerSettings(initialSelectedTime: nil)
-    //
-    //    var body: some View {
-    //        VStack {
-    //            Text("Настройте таймер игры:")
-    //                .foregroundStyle(Color(.black).opacity(0.8))
-    //            Picker("Время таймера", selection: $timerSettings.selectedTime) {
-    //                ForEach(1..<31, id: \.self) {
-    //                    Text("\($0) мин").tag($0)
-    //                }
-    //            }.pickerStyle(WheelPickerStyle())
-    //                .frame(width: 300, height: 300)
-    //            Button("Сохранить") {
-    //
-    //                timerSettings.updateSelectedTime(forKey: "timerSettings")
-    //
-    //            }
-    //            .font(.title)
-    //            .foregroundColor(.blue)
-    //            .padding(.bottom, 100)
-    //            .onAppear {
-    //                timerSettings.loadSettings(forKey: "timerSettings")
-    //            }
-    //        }
-    //        .frame(maxWidth: .infinity, maxHeight: .infinity)
-    //        .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
-    //        .navigationBarBackButtonHidden(true)
-    //        .toolbar {
-    //            ToolbarItem(placement: .navigationBarLeading) {
-    //                CustomBackButtonView()
-    //            }
-    //        }
-    //    }
-    //}
+//    class TimerSettings: ObservableObject {
+//    
+//        @Published var selectedTime: Int?
+//    
+//        init(initialSelectedTime: Int?) {
+//            self.selectedTime = initialSelectedTime
+//        }
+//        func updateSelectedTime(forKey: String) {
+//            UserDefaults.standard.set(selectedTime, forKey: forKey)
+//            print(time)
+//        }
+//        func loadSettings(forKey: String) {
+//            if let savedSettings = UserDefaults.standard.object(forKey: forKey) as? Int {
+//                selectedTime = savedSettings
+//                return savedSettings
+//            }
+//            selectedTime = 3
+//        }
+//    }
+//    
+//    struct TimerView: View {
+//    
+//        @StateObject var timerSettings = TimerSettings(initialSelectedTime: nil)
+//    
+//        var body: some View {
+//            VStack {
+//                Text("Настройте таймер игры:")
+//                    .foregroundStyle(Color(.black).opacity(0.8))
+//                Picker("Время таймера", selection: $timerSettings.selectedTime) {
+//                    ForEach(1..<31, id: \.self) {
+//                        Text("\($0) мин").tag($0)
+//                    }
+//                }.pickerStyle(WheelPickerStyle())
+//                    .frame(width: 300, height: 300)
+//                Button("Сохранить") {
+//    
+//                    timerSettings.updateSelectedTime(forKey: "timerSettings")
+//    
+//                }
+//                .font(.title)
+//                .foregroundColor(.blue)
+//                .padding(.bottom, 100)
+//                .onAppear {
+//                    timerSettings.loadSettings(forKey: "timerSettings")
+//                }
+//            }
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color(#colorLiteral(red:0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+//            .navigationBarBackButtonHidden(true)
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarLeading) {
+//                    CustomBackButtonView()
+//                }
+//            }
+//        }
+//    }
     
     //IV
     //
